d77b673ce776945f4d9e4102aad4dff52cf2cd7c
Matthijs Overboom <matthijsske@gmail.com>
2017-11-28 12:10:00 +0100

    Added 200 response to mollie webhook

--- src/AppBundle/Controller/MollieController.php
+++ src/AppBundle/Controller/MollieController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MollieController extends Controller
 {
@@ -12,9 +13,11 @@ class MollieController extends Controller
      * @Route(path="/mollie", name="mollie_webhook")
      *
      * @param Request $request
+     * @return Response
      */
     public function mollieWebhookAction(Request $request)
     {
         $this->get('logger')->debug($request->getContent());
+        return new Response();
     }
 }
