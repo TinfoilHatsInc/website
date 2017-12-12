f90c9736c337543fdf543113c8dec351adb8b2d6
Matthijs Overboom <matthijsske@gmail.com>
2017-12-12 14:14:13 +0100

    Only allow unauthenticated users on login page

---
+++ src/AppBundle/EventListener/InteractiveLoginListener.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthijs
+ * Date: 12-12-17
+ * Time: 14:02
+ */
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class InteractiveLoginListener
+{
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $session = $event->getRequest()->getSession();
+        dump($session);
+    }
+}
