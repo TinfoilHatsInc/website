518388844d4726f6ee2e84a04090c89fc0e0707d
Matthijs Overboom <matthijsske@gmail.com>
2017-12-18 11:23:29 +0100

    Fixed product listing at site + remove products via admin panel

--- src/AppBundle/Controller/Admin/ProductController.php
+++ src/AppBundle/Controller/Admin/ProductController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Security("has_role('ROLE_ADMIN')")
@@ -113,5 +114,29 @@ class ProductController extends Controller
             'form' => $form->createView(),
             'product' => $product
         ]);
+    }
+
+    /**
+     * @Route(path="/remove", name="admin_products_remove")
+     * @Method({"POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function removeAction(Request $request)
+    {
+        if(!$this->isCsrfTokenValid('remove_product_token', $request->get('csrf_token'))) {
+            return new Response("Invalid CSRF token", Response::HTTP_BAD_REQUEST);
+        }
+
+        $productId = $request->get('product_id');
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($productId);
+        if(!$product) {
+            return $this->redirectToRoute('admin_products');
+        }
+
+        $this->getDoctrine()->getManager()->remove($product);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('admin_products');
     }
 }
