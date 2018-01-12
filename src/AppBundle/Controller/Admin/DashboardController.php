43d717df65af935a07e4c19c1e9e62d4c0d51add
Matthijs Overboom <matthijsske@gmail.com>
2018-01-12 11:07:20 +0100

    Added simple widgets to admin dashboard

--- src/AppBundle/Controller/Admin/DashboardController.php
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller\Admin;
 
 
+use AppBundle\Entity\Chub;
+use AppBundle\Entity\Order;
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -26,6 +29,13 @@ class DashboardController extends Controller
      */
     public function indexAction()
     {
-        return $this->render(':admin:index.html.twig');
+        $totalOrders = count($this->getDoctrine()->getRepository(Order::class)->findAll());
+        $totalRegisteredChubs = count($this->getDoctrine()->getRepository(Chub::class)->findAll());
+        $totalProducts = count($this->getDoctrine()->getRepository(Product::class)->findAll());
+        return $this->render(':admin:index.html.twig', [
+            'totalOrders' => $totalOrders,
+            'totalRegisteredChubs' => $totalRegisteredChubs,
+            'totalProducts' => $totalProducts
+        ]);
     }
 }
